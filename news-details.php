a9690b6949abfb3096385fd57d03fdf7fa5633cc
joseCoronaL98 <127448697+joseCoronaL98@users.noreply.github.com>
2023-04-17 17:13:55 -0600

    Add files via upload

---
+++ news-details.php
@@ -0,0 +1,163 @@
+<?php
+session_start();
+include('includes/config.php');
+//Genrating CSRF Token
+if (empty($_SESSION['token'])) {
+    $_SESSION['token'] = bin2hex(random_bytes(32));
+}
+
+if (isset($_POST['submit'])) {
+    //Verifying CSRF Token
+    if (!empty($_POST['csrftoken'])) {
+        if (hash_equals($_SESSION['token'], $_POST['csrftoken'])) {
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $comment = $_POST['comment'];
+            $postid = intval($_GET['nid']);
+            $st1 = '0';
+            $query = mysqli_query($con, "insert into tblcomments(postId,name,email,comment,status) values('$postid','$name','$email','$comment','$st1')");
+            if ($query):
+                echo "<script>alert('comment successfully submit. Comment will be display after admin review ');</script>";
+                unset($_SESSION['token']);
+            else :
+                echo "<script>alert('Something went wrong. Please try again.');</script>";
+
+            endif;
+
+        }
+    }
+}
+$postid = intval($_GET['nid']);
+
+$sql = "SELECT viewCounter FROM tblposts WHERE id = '$postid'";
+$result = $con->query($sql);
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $visits = $row["viewCounter"];
+        $sql = "UPDATE tblposts SET viewCounter = $visits+1 WHERE id ='$postid'";
+        $con->query($sql);
+
+    }
+} else {
+    echo "no results";
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="es-MX">
+
+<head>
+
+    <title>Noticias | Inicio</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/modern-business.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+<!-- Navigation -->
+<?php include('includes/header.php'); ?>
+
+<!-- Page Content -->
+<div class="container">
+
+
+    <div class="row" style="margin-top: 4%">
+
+        <!-- Blog Entries Column -->
+        <div class="col-md-8">
+
+            <!-- Blog Post -->
+            <?php
+            $pid = intval($_GET['nid']);
+            $currenturl = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];;
+            $query = mysqli_query($con, "select tblposts.PostTitle as posttitle,tblposts.PostImage,tblcategory.CategoryName as category,tblcategory.id as cid,tblsubcategory.Subcategory as subcategory,tblposts.PostDetails as postdetails,tblposts.PostingDate as postingdate,tblposts.PostUrl as url,tblposts.postedBy,tblposts.lastUpdatedBy,tblposts.UpdationDate from tblposts left join tblcategory on tblcategory.id=tblposts.CategoryId left join  tblsubcategory on  tblsubcategory.SubCategoryId=tblposts.SubCategoryId where tblposts.id='$pid'");
+            while ($row = mysqli_fetch_array($query)) {
+                ?>
+
+                <div class="card mb-4">
+
+                    <div class="card-body">
+                        <h2 class="card-title"><?php echo htmlentities($row['posttitle']); ?></h2>
+
+                        <a class="badge bg-secondary text-decoration-none link-light"
+                           href="category.php?catid=<?php echo htmlentities($row['cid']) ?>"
+                           style="color:#fff"><?php echo htmlentities($row['category']); ?></a>
+
+                        <a class="badge bg-secondary text-decoration-none link-light"
+                           style="color:#fff"><?php echo htmlentities($row['subcategory']); ?></a>
+
+
+                        <p>
+
+                            <b>Publicado por </b> <?php echo htmlentities($row['postedBy']); ?>
+                            on </b><?php echo htmlentities($row['postingdate']); ?> |
+                            <?php if ($row['lastUpdatedBy'] != ''): ?>
+                            <b>última actualización </b> <?php echo htmlentities($row['lastUpdatedBy']); ?>
+                            on </b><?php echo htmlentities($row['UpdationDate']); ?></p>
+                        <?php endif; ?>
+                        <hr/>
+
+                        <img class="img-fluid rounded"
+                             src="admin/postimages/<?php echo htmlentities($row['PostImage']); ?>"
+                             alt="<?php echo htmlentities($row['posttitle']); ?>">
+
+                        <p class="card-text"><?php
+                            $pt = $row['postdetails'];
+                            echo(substr($pt, 0)); ?></p>
+
+                    </div>
+                    <div class="card-footer text-muted">
+
+
+                    </div>
+                </div>
+            <?php } ?>
+
+
+        </div>
+
+        <!-- Sidebar Widgets Column -->
+        <?php include('includes/sidebar.php'); ?>
+    </div>
+
+
+            <?php
+            $sts = 1;
+            $query = mysqli_query($con, "select name,comment,postingDate from  tblcomments where postId='$pid' and status='$sts'");
+            while ($row = mysqli_fetch_array($query)) {
+                ?>
+                <div class="media mb-4">
+                    <img class="d-flex mr-3 rounded-circle" src="images/usericon.png" alt="">
+                    <div class="media-body">
+                        <h5 class="mt-0"><?php echo htmlentities($row['name']); ?> <br/>
+                            <span style="font-size:11px;"><b>at</b> <?php echo htmlentities($row['postingDate']); ?></span>
+                        </h5>
+
+                        <?php echo htmlentities($row['comment']); ?>            </div>
+                </div>
+            <?php } ?>
+
+        </div>
+    </div>
+</div>
+
+
+<?php include('includes/footer.php'); ?>
+
+
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
